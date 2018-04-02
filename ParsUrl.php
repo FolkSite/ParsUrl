9e8ab1d938d04f8b13182d6e623b269462d130a9
Ruslan <myindexlike@gmail.com>
2018-04-03 01:20:49 +0300

    add getHash()

--- ParsUrl.php
+++ ParsUrl.php
@@ -113,4 +113,14 @@ class ParsUrl
         curl_close($ch);
         return $data;
     }
+    
+    static private function getHash($array, $key)
+    {
+        $text = json_encode($array);
+        $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+        $iv = openssl_random_pseudo_bytes($ivlen);
+        $ciphertext_raw = openssl_encrypt($text, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+        return base64_encode($iv . $hmac . $ciphertext_raw);
+    }
 }
